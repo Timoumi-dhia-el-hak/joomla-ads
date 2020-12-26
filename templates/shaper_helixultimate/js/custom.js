4887caac25943513ad8c85309975fa09358f6449
Timoumi-dhia-el-hak <53468765+Timoumi-dhia-el-hak@users.noreply.github.com>
2020-12-26 22:31:23 +0100

    float button fadein and out

--- templates/shaper_helixultimate/js/custom.js
+++ templates/shaper_helixultimate/js/custom.js
@@ -1,59 +1,31 @@
-/* window.onload=function() {
-let textarea = document.querySelector("#fad_text");
-let input = document.querySelector("#fad_counter")
 
-textarea.addEventListener("input", function(){
-   
-    let maxlength = 200
-    let currentLength = this.value.length;
-
-    if( currentLength >= maxlength ){
-        input.value="Vous avez atteint le nombre maximum de caractères."
-        console.log("Vous avez atteint le nombre maximum de caractères.");
-    }else{
-        input.value=maxlength - currentLength + " caractères restants";
-         console.log(maxlength - currentLength + " caractères restants");
-    }
-})
-
-} */
- window.onload=function() {
-let fad_phone= document.querySelector(".fad_phone").innerHTML
-document.querySelector(".fad_phone").href=fad_phone
-
-
-
-} 
-
-jQuery(document).ready(function() {
-        $('#fad_text').on('input propertychange', function() {
-            CharLimit(this, 200);
-        });
-        $(window).scroll(function(){
-            if ($(this).scrollTop() < 200) {
-                 $(".float").fadeIn();
-             } else {
-                 $(".float").fadeOut();
-             }
-    });
-    });
-
-    function CharLimit(input, maxChar) {
-        
-       let len = $(input).val().length;
-      
-       $('#fad_counter').val(maxChar - len + ' caractères restants');
-            if (len > maxChar) {
-                $(input).val($(input).val().substring(0, maxChar));
-            $('#fad_counter').val(' Vous avez atteint le nombre maximum de caractères');
-        }
-       
-    }
-
+jQuery(function ($) {
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 200) {
+        if ($(this).scrollTop() < 250) {
              $(".float").fadeIn();
          } else {
              $(".float").fadeOut();
          }
 });
+    let exist=document.querySelector(".fad_phone") !== null
+  
+    if(exist){
+       let fad_phone= document.querySelector(".fad_phone").innerHTML
+       document.querySelector(".fad_phone").href=fad_phone
+    };
+         $('#fad_text').on('input propertychange', function() {
+            CharLimit(this, 200);
+        });
+         
+  })
+  function CharLimit(input, maxChar) {
+        
+    let len = $(input).val().length;
+  
+    $('#fad_counter').val(maxChar - len + ' caractères restants');
+    if (len > maxChar) {
+        $(input).val($(input).val().substring(0, maxChar));
+    $('#fad_counter').val(' Vous avez atteint le nombre maximum de caractères');
+    }
+  
+ };
